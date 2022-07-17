d6d3478c8703d5ab8f0e6f9d4316e0f0d19a357f
Uriel Moya Mata <moyauriel05@gmail.com>
2022-07-17 17:29:00 -0500

    Finished botons

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const sections    = document.querySelectorAll('.sections');
+const sections    = document.querySelectorAll('.section');
 const sectBtns    = document.querySelectorAll('.controlls');
 const sectBtn     = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
@@ -15,6 +15,27 @@ function PageTransitions()
 			this.className += " active-btn"
 		})
 	}
+
+	//Sections Active Class
+	allSections.addEventListener('click', (e) => {
+		const id = e.target.dataset.id;
+		if(id)
+		{
+			//Remove selected from the other btns
+			sectBtns.forEach((btn) => {
+				btn.classList.remove('active')
+			})
+			e.target.classList.add('active')
+
+			//Hide other sections
+			sections.forEach((section) => {
+				section.classList.remove('active')
+			})
+
+			const element = document.getElementById(id);
+			element.classList.add('active');
+		}
+	})
 }
 
 PageTransitions();
