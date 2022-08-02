bd97ffaf06ed765c3b22c0b802094ca008bf5cc9
Uriel Moya Mata <moyauriel05@gmail.com>
2022-08-02 17:21:47 -0500

    Finished 'contact section'

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ function PageTransitions()
 			element.classList.add('active');
 		}
 	})
+
+	//Toggle theme
+	const themeBtn = document.querySelector('.theme-btn');
+	themeBtn.addEventListener('click',() => {
+		let element = document.body;
+		element.classList.toggle('light-mode');
+	});
 }
 
 PageTransitions();
